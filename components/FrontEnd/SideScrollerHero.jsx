c672c81ece16421545e4cc7ac7df2b630ae1c5ae
lee14k <kmkovach@mtu.edu>
2024-08-26 19:14:22 -0400

    working the side scroller

--- components/FrontEnd/SideScrollerHero.jsx
+++ components/FrontEnd/SideScrollerHero.jsx
@@ -2,20 +2,39 @@ import StarIcon from '@mui/icons-material/Star';
 export default function SideScrollerHero() {
     return (
         <div className="scroll-container">
-            <ul className="carousel-primary">
-                <li>Spacious rooms</li>
-                <li><StarIcon/></li>
-                <li>Open year round</li>
-                <li><StarIcon/></li>
-                <li>Picnic tables and grills on the shore of Lake Michigan</li>
-                <li><StarIcon/></li>
-                <li>Easy access to the snowmobile trails</li>
-                <li><StarIcon/></li>
-                <li>Affordable rates</li>
-                <li><StarIcon/></li>
-                <li>Free WiFi</li>
-                <li><StarIcon/></li>
-            </ul>
+            <div className="carousel-primary">
+                <ul className="flex">
+                    <li>Spacious rooms</li>
+                    <li><StarIcon/></li>
+                    <li>Open year round</li>
+                    <li><StarIcon/></li>
+                    <li>Picnic tables and grills on the shore of Lake Michigan</li>
+                    <li><StarIcon/></li>
+                    <li>Easy access to the snowmobile trails</li>
+                    <li><StarIcon/></li>
+                    <li>Affordable rates</li>
+                    <li><StarIcon/></li>
+                    <li>Free WiFi</li>
+                    <li><StarIcon/></li>
+                </ul>
+            </div>
+
+            <div className="carousel-secondary carousel-primary">
+                <ul className="flex">
+                    <li>Spacious rooms</li>
+                    <li><StarIcon/></li>
+                    <li>Open year round</li>
+                    <li><StarIcon/></li>
+                    <li>Picnic tables and grills on the shore of Lake Michigan</li>
+                    <li><StarIcon/></li>
+                    <li>Easy access to the snowmobile trails</li>
+                    <li><StarIcon/></li>
+                    <li>Affordable rates</li>
+                    <li><StarIcon/></li>
+                    <li>Free WiFi</li>
+                    <li><StarIcon/></li>
+                </ul>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Hero from "../components/FrontEnd/Hero";
 import TriplePhotoStarBackground from "../components/FrontEnd/TriplePhotoStarBackground";
-
+import SideScrollerHero from "@/components/FrontEnd/SideScrollerHero";
 
 export default function Home() {
   return (
    <div>
      <Hero/>
+       <SideScrollerHero/>
      <TriplePhotoStarBackground/>
    </div>
   );
