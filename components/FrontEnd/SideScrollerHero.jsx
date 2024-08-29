532dd056852237e1695090066ec06d7f8e7d0e49
lee14k <kmkovach@mtu.edu>
2024-08-29 12:13:23 -0400

    removing grills

--- components/FrontEnd/SideScrollerHero.jsx
+++ components/FrontEnd/SideScrollerHero.jsx
@@ -10,8 +10,6 @@ export default function SideScrollerHero() {
                     <li><StarIcon/></li>
                     <li>Open year round</li>
                     <li><StarIcon/></li>
-                    <li>Picnic tables and grills on the shore of Lake Michigan</li>
-                    <li><StarIcon/></li>
                     <li>Easy access to the snowmobile trails</li>
                     <li><StarIcon/></li>
                     <li>Affordable rates</li>
@@ -25,8 +23,6 @@ export default function SideScrollerHero() {
                     <li>Spacious rooms</li>
                     <li><StarIcon/></li>
                     <li>Open year round</li>
-                    <li><StarIcon/></li>
-                    <li>Picnic tables and grills on the shore of Lake Michigan</li>
                     <li><StarIcon/></li>
                     <li>Easy access to the snowmobile trails</li>
                     <li><StarIcon/></li>
